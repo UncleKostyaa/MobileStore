46ffd3d137f26f9ea56e5aa558db5ca287fe377d
UncleKostyaa <unclekostya7670@outlook.com>
2025-11-02 15:58:08 +0200

    added badged box

--- app/src/main/java/com/unclekostya/bookstore/MainActivity.kt
+++ app/src/main/java/com/unclekostya/bookstore/MainActivity.kt
@@ -5,13 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgeDefaults
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -24,11 +32,16 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -69,7 +82,10 @@ class MainActivity : ComponentActivity() {
                 val cartViewModel: CartViewModel = viewModel(factory = CartViewModelFactory(repository = repository))
                 Scaffold(
                     topBar = {
-                        TopBar(navController = navController)
+                        TopBar(
+                            navController = navController,
+                            cartViewModel = cartViewModel
+                        )
                     }
 
                 ) { innerPadding ->
@@ -93,41 +109,60 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TopBar(
     navController: NavHostController,
+    cartViewModel: CartViewModel,
     modifier: Modifier = Modifier
 ) {
+    val cartState = cartViewModel.cart.value
+    val cartItems = cartState?.listOfProductsId?.size ?: 0
+
     CenterAlignedTopAppBar(
         title = {
+            Text(
+                text = "Tech Store",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        actions = {
             Box(
-                modifier = modifier
+                modifier = Modifier
                     .padding(end = 16.dp)
-                    .fillMaxSize()
-                ) {
-                Text(
-                    text = "Tech Store",
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = modifier
-                        .align(Alignment.Center)
+            ) {
+                BadgedBox(
+                    badge = {
+                        Badge(
+                            containerColor = Color.Red,
+                            modifier = Modifier
+                                .padding(top = 30.dp, start = 2.dp)
 
-                )
-                IconButton(
-                    onClick = {
-                        navController.navigate("cart") {
-                            popUpTo(navController.graph.startDestinationId) { saveState = true }
-                            launchSingleTop = true
-                            restoreState = true
+                        ) {
+                            Text(
+                                text = "$cartItems",
+                                color = Color.White,
+                                fontSize = 10.sp,
+                                fontWeight = FontWeight.Bold
+                            )
                         }
                     },
                     modifier = modifier
-                        .align(Alignment.CenterEnd)
-
                 ) {
-                    Icon(
-                        painter = painterResource(R.drawable.cart_white),
-                        contentDescription = "cart",
+                    IconButton(
+                        onClick = {
+                            navController.navigate("cart") {
+                                popUpTo(navController.graph.startDestinationId) { saveState = true }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        },
                         modifier = modifier
-                            .size(38.dp)
-                    )
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.cart_white),
+                            contentDescription = "Cart",
+                            modifier = Modifier.size(38.dp),
+                            tint = Color.Black
+                        )
+                    }
                 }
             }
         },
